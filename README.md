2d15f6ea93434dde63de8e27b7545e171b90ad49
mlmedia <mike@millionlightsmedia.com>
2020-07-03 02:36:50 -0400

    - initial commit - moving old code from demo-public catch-all repo

---
+++ README.md
@@ -0,0 +1,6 @@
+#  Tutorial for NodeJS and Express
+Tutorial for NodeJS and Express.  Tutorial origin unknown but repo can be found here: [https://github.com/GainorB/node-auth-jwt](https://github.com/GainorB/node-auth-jwt).
+
+
+## NOTE
+This is an old project and just keeping it here for posterity purposes.
---
+++ build/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _Reflection = require('./src/controllers/Reflection');
+
+var _Reflection2 = _interopRequireDefault(_Reflection);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)(); // server.js
+
+
+app.use(_express2.default.json());
+
+app.get('/', function (req, res) {
+	return res.status(200).send({
+		'message': 'YAY! Congratulations! Your first endpoint is working'
+	});
+});
+
+app.post('/api/v1/reflections', _Reflection2.default.create);
+app.get('/api/v1/reflections', _Reflection2.default.getAll);
+app.get('/api/v1/reflections/:id', _Reflection2.default.getOne);
+app.put('/api/v1/reflections/:id', _Reflection2.default.update);
+app.delete('/api/v1/reflections/:id', _Reflection2.default.delete);
+
+app.listen(3000);
+console.log('app running on port ', 3000);
